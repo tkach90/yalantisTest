ac7d92f8793ac1406a0d4e09ef37e58ac4a459ad
thach90 <tkach90@yahoo.com.ua>
2020-01-06 10:35:58 +0200

    output user list using axios, made user selection by month from date of birth using moment js, start to implement color changing logic

---
+++ src/components/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+import dateToMonth from "../helpers/dateToMonth";
+
+class App extends Component {
+    monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            filteredUsers: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`https://yalantis-react-school.herokuapp.com/api/task0/users`)
+            .then(response => {
+                this.setState({ users: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    setMonth = (event) => {
+        this.setState({ filteredUsers: this.state.users.filter((user) => {
+                return event.target.innerText === dateToMonth(user.dob);
+            })
+        });
+
+    };
+
+    clearMonth = () => this.setState({ filteredUsers: [] });
+
+    setColor = (month) => {
+        const { users } = this.state;
+
+        const length = users.filter((user) => {
+            return month === dateToMonth(user.dob);
+        }).length;
+
+        if ( length < 3 ) {
+            return 'gray';
+        } else if ( 2 < length < 7) {
+            return 'blue';
+        } else if ( 6 < length < 11) {
+            return 'green';
+        } else if ( 10 < length) {
+            return 'red';
+        }
+    };
+
+    render() {
+        const { users } = this.state;
+
+        return (
+            <>
+                <div>
+                    {users && this.monthNames.map(month =>
+                        {console.log('month', month)}
+                            // <div
+                            //     onMouseOver={this.setMonth}
+                            //     onMouseOut={this.clearMonth}
+                            //     className={this.setColor(month)}
+                            // >
+                            //     {month}
+                            // </div>
+                    )}
+                </div>
+                {this.state.filteredUsers && this.state.filteredUsers.map(user =>
+                    <div key={user.id}>
+                        {user.firstName}
+                        {' '}
+                        {user.lastName}
+                        {': '}
+                        {user.dob}
+                    </div>
+                )}
+            </>
+        );
+    }
+}
+
+export default App;
