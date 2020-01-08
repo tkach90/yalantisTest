045bc7b07e144c6386564098a00d781899c6bd8e
thach90 <tkach90@yahoo.com.ua>
2020-01-08 07:53:10 +0200

    create axios config, add stylization using styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,63 @@
 import React, { Component } from 'react';
-
 import axios from 'axios';
+import styled from 'styled-components';
+
+import axiosConfig from '../api/axiosConfig';
 import dateToMonth from "../helpers/dateToMonth";
+
+const MonthWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;  
+    
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;    
+        width: 100px;
+        height: 60px;
+        margin: 5px;
+        border-radius: 6px;
+        box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.3), 
+              -3px -3px 6px rgba(255, 255, 255, 0.3), 
+              inset 2px 2px 5px rgba(255, 255, 255, 0.3), 
+              inset -2px -2px 5px rgba(0, 0, 0, 0.3);
+    }
+    
+    .red {
+      background: red;
+      color: #fff;
+    }
+    
+    .green {
+      background: green;
+      color: #fff;
+    }
+    
+    .blue {
+      background: blue;
+      color: #fff;
+    }
+    
+    .gray {
+      background: gray;
+      color: #fff;
+    }
+`;
+
+
+const UsersWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px auto 0;
+  justify-content: center;
+  width: 400px;
+  padding: 3px 10px 3px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 6px rgba(0, 0, 0, 0.3), 
+              -3px -3px 6px rgba(255, 255, 255, 0.3), 
+              inset 2px 2px 5px rgba(255, 255, 255, 0.3), 
+              inset -2px -2px 5px rgba(0, 0, 0, 0.3);
+`;
 
 class App extends Component {
     monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -15,7 +71,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        axios.get(`https://yalantis-react-school.herokuapp.com/api/task0/users`)
+        axiosConfig.get('/users' )
             .then(response => {
                 this.setState({ users: response.data });
             })
@@ -57,9 +113,10 @@ class App extends Component {
 
         return (
             <>
-                <div>
-                    {!!users.length && this.monthNames.map(month =>
+                <MonthWrapper>
+                    {!!users.length && this.monthNames.map((month, index) =>
                         <div
+                            key={index}
                             onMouseOver={this.setMonth}
                             onMouseOut={this.clearMonth}
                             className={this.setColor(month)}
@@ -67,15 +124,15 @@ class App extends Component {
                             {month}
                         </div>
                     )}
-                </div>
+                </MonthWrapper>
                 {this.state.filteredUsers && this.state.filteredUsers.map(user =>
-                    <div key={user.id}>
+                    <UsersWrapper key={user.id}>
                         {user.firstName}
                         {' '}
                         {user.lastName}
                         {': '}
                         {user.dob}
-                    </div>
+                    </UsersWrapper>
                 )}
             </>
         );
